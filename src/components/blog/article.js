cc944f5f0d7fec8125210adda9be7584c0b7f9c8
Anastasiya <tyshkavets@gmail.com>
2020-07-02 16:42:47 +0100

    feat(article): add default image to the top of the article template

--- src/components/blog/article.js
+++ src/components/blog/article.js
@@ -3,13 +3,17 @@ import PropTypes from "prop-types"
 import makeStyles from "@material-ui/core/styles/makeStyles"
 import Typography from "@material-ui/core/Typography"
 import grey from "@material-ui/core/colors/grey"
+import Img from "gatsby-image"
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(7.5, 0, 5),
   },
   header: {
-    marginBottom: theme.spacing(5),
+    marginBottom: theme.spacing(4),
+  },
+  img: {
+    marginBottom: theme.spacing(4),
   },
   title: {
     fontSize: "3rem",
@@ -42,6 +46,7 @@ const useStyles = makeStyles(theme => ({
 
 const Article = ({ post }) => {
   const classes = useStyles()
+  const img = post.frontmatter.image
 
   return (
     <article className={classes.root}>
@@ -53,6 +58,12 @@ const Article = ({ post }) => {
           Updated on {post.frontmatter.updatedOnDate}
         </p>
       </header>
+      {img && (
+        <Img
+          className={classes.img}
+          fluid={post.frontmatter.image.childImageSharp.fluid}
+        />
+      )}
       <section
         dangerouslySetInnerHTML={{ __html: post.html }}
         className={classes.text}
