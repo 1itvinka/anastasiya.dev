5405cd3a459e9a5e05e83b23f2acb37da82d386e
Anastasiya <tyshkavets@gmail.com>
2020-09-25 16:39:22 +0100

    fix(description): minor text changes

--- src/components/description.js
+++ src/components/description.js
@@ -17,8 +17,8 @@ export default function Description() {
   return (
     <Typography variant="body1" className={classes.text}>
       It's not a traditional blog with a number of static posts. It's a space
-      where I collect ideas, resources, snippets and just random bits and
-      pieces. It's a Digital Garden that is evolving in real time.{" "}
+      where I collect ideas, resources, snippets, and just random bits and
+      pieces. It's a Digital Garden that is evolving in real-time.{" "}
       <Link href="/why-digital-garden/" underline="none">
         More about it.
       </Link>
